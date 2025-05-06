17531c28f041ca3b569e7d239b8013dd9855604b
alperyzx <alperstarr@yahoo.com>
2025-05-06 14:25:45 +0300

    Update version number to v2.5 in footer of parewebMon

--- parewebMon.py
+++ parewebMon.py
@@ -1122,7 +1122,7 @@ async def welcome_page():
             
             <div class="welcome-footer" style="margin-top: 40px; text-align: center; color: #777;">
                 <p>Select a section above to start working with your Redis cluster</p>
-                <p style="font-size: 12px;">Paredicma v2.0 - Redis Cluster Management Tool</p>
+                <p style="font-size: 12px;">Paredicma v2.5 - Redis Cluster Management Tool</p>
             </div>
         </div>
     </body>
